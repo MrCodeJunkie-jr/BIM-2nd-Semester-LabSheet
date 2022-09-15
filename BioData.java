7cc45687e3c29c834ce3aeafb8de8ad7f46f8c55
Pratik Raut <106788839+MrCodingJunky-jr@users.noreply.github.com>
2022-09-15 07:07:06 +0545

    Add files via upload

---
+++ BioData.java
@@ -0,0 +1,12 @@
+class BioData
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Pratik Raut");
+		System.out.println("Address: Dhapakhel,Lalitpur");
+		System.out.println("DOB: 09/12/2003");
+		System.out.println("Gender: M");
+		System.out.println("Email: rautp640@gmail.com");
+		System.out.println("Phone: 9880326460");
+	}
+}
