d6598d39f315fd442ba2299a6bd24377b013ac96
Pratik Raut <106788839+MrCodingJunky-jr@users.noreply.github.com>
2022-09-20 08:58:19 +0545

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -5,22 +5,19 @@ class SimpleInterest
 	
 		{
 			Scanner scan = new Scanner(System.in);
-			double p;
-			double t;
-			double r; 
-			double SI;
+		 
 			System.out.println("Enter principle,time and rate:");
 			
-			System.out.println("p:");
-			p=scan.nextDouble();
+			System.out.println("Principle:");
+			double p=scan.nextDouble();
 			
-			System.out.println("t:");
-			t=scan.nextDouble();
+			System.out.println("Time:");
+			double t=scan.nextDouble();
 			
-			System.out.println("r:");
-			r=scan.nextDouble();
+			System.out.println("Rate:");
+			double r=scan.nextDouble();
 			
-			SI=(p*t*r)/100;
+			double SI=(p*t*r)/100;
 			
 			System.out.println("Simple Interest=" +SI);
 		}
